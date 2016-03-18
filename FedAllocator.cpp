3d0a1ea6804114a829afb4d8789cbab720f0cdc2
Parshuram <parushuram.k@huawei.com>
2016-03-18 13:33:44 +0000

    Initial copy of FederationAllocator class defination with module creation

---
+++ FedAllocator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <mesos/master/allocator.hpp>
+#include <mesos/module/allocator.hpp>
+#include <stout/try.hpp>
+
+#include "FedAllocator.hpp"
+
+using namespace mesos;
+using mesos::master::allocator::Allocator;
+
+
+void FederationAllocator::addFramework(const FrameworkID& frameworkId, 
+				   const FrameworkInfo& frameworkInfo, 
+				   const hashmap<SlaveID, Resources>& used)
+{
+    std::cout << "   :::::: HC#03 -- OUR addFramework method is called" << std::endl;
+}
+
+
+static Allocator* createAllocator(const Parameters& parameters)
+{
+    std::cout << "   :::::: HC#01 --  BEFORE createAllocator()" << std::endl;
+
+    Try<Allocator*> allocator = FilterOffersFedAlloc::create();
+
+    if (allocator.isError()) 
+    {
+       return NULL;
+    }
+    std::cout << "   :::::: HC#02 --  AFTER createAllocator()" << std::endl;
+
+    return allocator.get();
+}
+
+
+mesos::modules::Module<Allocator> FedAllocatorModule (MESOS_MODULE_API_VERSION, MESOS_VERSION, "Huawei Mesos Federation Project", "parushuram.k@huawei.com", "Cloud Federation Allocator Module.", NULL, createAllocator);
+
